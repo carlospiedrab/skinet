6216352d1c0c7dd95e39839400bcd3c131e98b64
Carlos Piedra <barce90@hotmail.com>
2021-10-07 21:40:36 -0700

    Initial Commit Oct-2021

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "API", Version = "v1" });
+});
+
+builder.Services.AddDbContext<StoreContext>(options => 
+                                            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Core/Entities/Product.cs
@@ -0,0 +1,13 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+            Name="";
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
