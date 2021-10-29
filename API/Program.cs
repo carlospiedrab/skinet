18d97df1665fb8137868940a59980e01e2233b98
Carlos Piedra <barce90@hotmail.com>
2021-10-28 20:24:40 -0700

    End of Section 8

--- API/Program.cs
+++ API/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy => 
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
     });
 });
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'Skinet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {
+    this.products = [];
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7206/api/products?pageSize=50').subscribe((response: any) =>
+    {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
