096bc4b81c70444eda8b7db8b1e743b83f7a1c89
carlospiedrab <barce90@hotmail.com>
2022-04-24 09:49:21 -0700

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { delay, map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -40,6 +41,10 @@ getProducts(shopParams: ShopParams){
             return response.body;
           })
         );
+}
+
+getProduct(id: number){
+  return this.http.get<IProduct>(this.baseUrl + 'products/'+id);
 }
 
 getBrands(){
